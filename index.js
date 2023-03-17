810fdafd6b547c38c9958b4fd9f66ace0631e019
Shawn Webster <swolfgangwebster@gmail.com>
2023-03-17 12:41:12 -0500

    fair

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(arr) {
+    return [...arr].slice(0,2);
+}
+
+const returnLastTwoDrivers = function(arr) {
+    return [...arr].slice(arr.length-2,arr.length);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(i) {
+    return (d) => {return i*d}
+}
+
+const fareDoubler = createFareMultiplier(2)
+
+const fareTripler = createFareMultiplier(3)
+
+function selectDifferentDrivers(d,s) {
+    return s(d)
+}
+
+
+
